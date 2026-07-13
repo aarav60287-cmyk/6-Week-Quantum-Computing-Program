9ebf4290aa10bcf7abd053bfa8416446758a8b1a
HS01 <hs01@Skills-Academy-58.local>
2026-07-13 11:12:08 -0700

    Learned range in Python

---
+++ First.py
@@ -0,0 +1,11 @@
+number = 5
+code = "Hello"
+at_dae = True
+
+if number > 3:
+    print("Yes, it's greater than 3")
+else:
+    print("It's not greater than 3")
+
+Name = input("What is your name? ")
+print("Hello " + Name + "!")
---
+++ Range.py
@@ -0,0 +1,2 @@
+for number in range(3000000000):
+    print(number)
